6bd12539ce2650b73e68ab16ce15935921b81a15
Marcelo da Silva <marceloxhenrique@hotmail.com>
2023-06-02 20:15:38 +0200

    Update Page Skillls

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,18 @@ import styles from "./Home.module.css";
 // import profilePicture from "../assets/profilePicture.png";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import 'animate.css';
+import { useState } from "react";
+
 export default function Home() {
+
+  const [animationHinge, setAnimationHinger] = useState(false)
   return (
     <div className={styles.homeContainer} id="home">
       <div className={styles.leftSide}>
-        <h1 className={styles.title}>
-          Hi, I{"’"}m <br /> Marcelo
-        </h1>
-        <p>a  Web Developer <br /> Who build things for the web</p>
+        <p>Hi, I{"’"}m Marcelo,</p>
+        <h1 className={styles.title}>Web Developer</h1>
+        <p>Who build things for the web</p>
         <div className={styles.iconContainer}>
           <a
             href="https://github.com/marceloxhenrique"
@@ -27,8 +31,20 @@ export default function Home() {
           </a>
         </div>
       </div>
-      <div className={styles.rightSide}>
-        <div className={styles.picture}></div>
+      <div
+        className={styles.rightSide}
+      >
+        <div
+          className={animationHinge && `animate__animated animate__hinge`}
+          onClick={() => {
+            setAnimationHinger(!animationHinge);
+          }}
+        >
+          <div
+            className={styles.picture}
+            id={animationHinge ? undefined : styles["animation"]}
+          ></div>
+        </div>
       </div>
     </div>
   );
