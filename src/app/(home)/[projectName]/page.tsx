ed2f50282becabebb2bfbed7d65d0973484e6c29
Marcelo da Silva <marceloxhenrique@hotmail.com>
2024-01-02 23:52:21 +0100

    refactor: update class Project constructor, add more images to Projectexternatic

--- src/app/(home)/[projectName]/page.tsx
+++ src/app/(home)/[projectName]/page.tsx
@@ -1,55 +1,93 @@
 "use client";
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 import { useParams } from "next/navigation";
 import { Project } from "@/components/ProjectsList";
 import { IoReturnDownBack } from "react-icons/io5";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import Link from "next/link";
 
 const Projects = () => {
+  const [picture, setPicture] = useState<string>();
+  const [zoomInOut, setZoomInOut] = useState<boolean>(false);
   const { projectName } = useParams();
   const projectDecode = decodeURI(projectName.toString());
   const [projectFiltered] = Project.filter(
     (project) => project.title == projectDecode,
   );
 
+  const zoomPicture = (data: string) => {
+    setPicture(data);
+    setZoomInOut(!zoomInOut);
+  };
+
   return (
-    <main className="flex min-h-screen w-full flex-col  ">
-      <Link href={"/"}>
-        <Button className="my-4 w-16 text-xl">
-          <IoReturnDownBack className="h-12 w-12 cursor-pointer" />
-        </Button>
+    <main className="flex min-h-screen w-full flex-col ">
+      <Link
+        href={"/"}
+        className="my-4 flex w-16 items-center justify-center text-xl"
+      >
+        <IoReturnDownBack className="h-12 w-12 cursor-pointer" />
       </Link>
-      <section className="flex w-full flex-col items-center justify-between  p-4 lg:flex-row">
-        <div className="">
-          <h1 className="mt-4 text-4xl">{projectFiltered.title}</h1>
-          <p className="max-w-[30rem] py-8 text-xl">{projectFiltered.text}</p>
+      <section className="flex w-full flex-col items-center justify-between gap-2 p-4 lg:flex-row lg:items-start">
+        <article className="flex flex-col items-center ">
+          <h1 className=" w-full text-left text-4xl">
+            {projectFiltered.title}
+          </h1>
           <div className="my-4 max-w-[30rem]">
             <Image
               src={projectFiltered.normalImage}
               alt={`${projectFiltered.title} image`}
               className=""
             />
+            <p className="p-4 text-justify text-lg">
+              {projectFiltered.description}
+            </p>
           </div>
-        </div>
-        <div className="relative grid max-w-[50rem] grid-cols-3 gap-4 overflow-hidden rounded-md bg-slate-200 p-2">
-          {projectFiltered.mockupImages.map((project) => (
-            <Image
-              key={project.src}
-              src={project.src}
-              className="rounded-md bg-white hover:absolute hover:left-0 hover:top-0 hover:h-full hover:w-full"
-              width={200}
-              height={200}
-              alt={`${projectFiltered.title} image`}
-            />
-          ))}
+        </article>
+        <div className="max-w-[40rem]">
+          <section className="relative">
+            <div className=" grid max-w-[40rem] grid-cols-3 gap-4 overflow-hidden rounded-md bg-slate-200 p-2">
+              {projectFiltered.mockupImages.map((project) => (
+                <Image
+                  key={project.src}
+                  src={project.src}
+                  className={`h-full cursor-pointer rounded-md bg-white`}
+                  width={200}
+                  height={200}
+                  alt={`${projectFiltered.title} image`}
+                  onClick={() => {
+                    zoomPicture(project.src);
+                  }}
+                />
+              ))}
+            </div>
+            {picture && (
+              <Image
+                src={picture}
+                className={`${
+                  zoomInOut
+                    ? `absolute left-0 top-0 h-full w-full p-1`
+                    : `hidden`
+                }   cursor-pointer rounded-md bg-white`}
+                width={200}
+                height={200}
+                alt={`${projectFiltered.title} image`}
+                onClick={() => {
+                  setZoomInOut(!zoomInOut);
+                }}
+              />
+            )}
+          </section>
+          <h2 className="pt-4 text-lg underline ">
+            Tech used in this project:
+          </h2>
+          <p className="w-fit rounded-md  py-4 text-xl font-semibold">
+            {projectFiltered.thecnologies}
+          </p>
         </div>
       </section>
-      <h2>Tech used in this porjects</h2>
-      <p className="w-fit rounded-md bg-slate-300 text-xl font-semibold">
-        {projectFiltered.thecnologies}
-      </p>
+      {/* <h2 className="pl-4">Tech used in this porjects</h2> */}
     </main>
   );
 };
--- src/components/ProjectsList.ts
+++ src/components/ProjectsList.ts
@@ -5,6 +5,9 @@ import externatic1 from "../../public/externatic1.png";
 import externatic2 from "../../public/externatic2.png";
 import externatic3 from "../../public/externatic3.png";
 import externatic4 from "../../public/externatic4.png";
+import externatic5 from "../../public/externatic5.png";
+import externatic6 from "../../public/externatic6.png";
+import externatic7 from "../../public/externatic7.png";
 import wildQuizDesktop from "../../public/wildQuiz-desktop.png";
 import wildQuizMobile from "../../public/wildQuiz-mobile.png";
 import wildQuiz from "../../public/wildQuiz.png";
@@ -16,6 +19,7 @@ class Projects {
     public id: number,
     public title: string,
     public text: string,
+    public description: string,
     public imageMobile: StaticImageData,
     public imageDesktop: StaticImageData,
     public normalImage: StaticImageData,
@@ -29,22 +33,27 @@ class Projects {
 const Projectexternatic: Projects = new Projects(
   1,
   "Externatic",
-  "In collaboration with Externatic, we have successfully developed a web and mobile application designed to streamline job searches for candidates. This marks my final project during the web developer training at the Wild Code School.",
+  "In collaboration with Externatic, we have successfully developed a web and mobile application designed to streamline job searches for candidates. This   marks my final project during the web developer training at the Wild Code School.",
+  `On the Frontend, we developed an intuitive user-friendly and responsive design interface using  technologies such as HTML5, CSS3, and JavaScript. The dynamism of the application was achieved through the implementation of ReactJS, ensuring a seamless user experience on both web and mobile platforms.
+  For the Backend, we used NodeJS and ExpressJS, to build a server infrastructure
+  In terms of the database, we implemented a SQL database to efficiently store and retrieve data.
+  To bring our vision to life, we collaborated on the design aspects using Figma, creating a visually appealing and cohesive identity for the application. The use of Mui/material further enriched the user interface, providing a polished and modern aesthetic.
+  This project not only honed my technical skills but also underscored the importance of team work`,
   externaticMobile,
   externaticDesktop,
   externatic,
   "https://github.com/WildCodeSchool/2023-02-JS-FT-Lyon-P3-Externatic",
   "https://externatic.lyon-2.wilders.dev/",
-  "HTML5 - CSS3 - JavaScript - ReactJS - NodeJS - ExpressJS - SQL",
+  "HTML5 - CSS3 - JavaScript - ReactJS - NodeJS - ExpressJS - SQL - Mui/material - Figma",
   [
     externatic1,
     externatic2,
     externatic3,
     externatic4,
-    externatic1,
-    externatic2,
-    externatic3,
-    externatic4,
+    externatic5,
+    externatic6,
+    externatic7,
+    // externatic4,
   ],
 );
 
@@ -52,12 +61,13 @@ const ProjectWildQuiz: Projects = new Projects(
   2,
   "Wild Quiz",
   "Wild Quiz is a Quizz about the city of Lyon, this Project is the first of the three projects created during the course of Web Development Of The Wild Code School of Lyon",
+  `The project consists of an interactive quiz based on the City of Lyon. The user can explore a dynamic map of Lyon, on the map points of interest are marked, and the user can click on these points to start the quiz.`,
   wildQuizMobile,
   wildQuizDesktop,
   wildQuiz,
   "https://github.com/WildCodeSchool/2023-02-JS-FT-Lyon-P1-WildQuiz",
-  "Wild Quiz",
-  "",
+  "https://wildcodeschool.github.io/2023-02-JS-FT-Lyon-P1-WildQuiz/",
+  "HTML5 - CSS3 - JavaScript - Leaflet - Figma",
   [externatic1, externatic2, externatic3, externatic4],
 );
 export const Project: Projects[] = [Projectexternatic, ProjectWildQuiz];
