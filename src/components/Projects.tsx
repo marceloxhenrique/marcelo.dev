749a689e6d0f86cc81b35449347c488176f69442
Marcelo da Silva <marceloxhenrique@hotmail.com>
2024-10-08 23:20:32 +0200

    feat: Add dummyDataAPI project to the list projects.

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -8,10 +8,10 @@ const Projects = () => {
       id="PROJECTS"
       className="flex min-h-screen w-full flex-col justify-between pt-20 md:mt-40"
     >
-      <h1 className=" text-center text-4xl font-semibold md:text-5xl">
+      <h1 className="mb-8 text-center text-4xl font-semibold md:text-5xl">
         PROJECTS
       </h1>
-      <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+      <div className="grid grid-cols-1 gap-12 md:grid-cols-2">
         {Project.map((project) => (
           <CardProject key={project.id} project={project} />
         ))}
